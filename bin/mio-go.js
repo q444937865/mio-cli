6a338489668b989e71878f735208b412e9a184dd
zz <4449378965@qq.com>
2020-03-10 11:58:28 +0800

    fix: lose path

--- bin/mio-go.js
+++ bin/mio-go.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const path = require("path");
 const program = require("commander");
 const webpack = require("webpack");
 const webpackDevServer = require("webpack-dev-server");
